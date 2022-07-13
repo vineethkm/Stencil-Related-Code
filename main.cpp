1bf38cd3f59d3ac1d5a7d0efb7ed839e820fba84
vineethkm <vineethkm.01@gmail.com>
2022-07-13 19:02:42 +0530

    Added profiling data

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
+#include<array>
 #include "lattice.h"
 
 using namespace lattice;
 int main()
 {
-    int grid [10][10][10];
+    int n = 10;
+    auto itr = lattice_iterator<3>({{0,0,0}},{{n,n,n}});
+    lattice_iterator<3> itr_end;
+    for(;itr != itr_end; itr++)
+    {
+        std::cout<<(*itr)[0]<<(*itr)[1]<<(*itr)[2]<<std::endl;
+    }
 
     return 0;
 }
